d63439df296f128121caabe4562ed75cbb1ca231
pavel <pavel.somov@icloud.com>
2023-05-22 21:46:44 +0300

    Finish project

--- KR1/Controllers/HomeController.cs
+++ KR1/Controllers/HomeController.cs
@@ -1,20 +1,23 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using KR1.Models;
-using System.ComponentModel.DataAnnotations;
 
 namespace KR1.Controllers
 {
     public class HomeController : Controller
     {
-        ApplicationContext db;
+        private readonly ApplicationContext _db;
+
         public HomeController(ApplicationContext context)
         {
-            db = context;
+            _db = context;
         }
+
         public IActionResult Index()
         {
-            return View(db.Applications.ToList());
+            var applications = _db.Applications.ToList();
+            return View(applications);
         }
 
         [HttpGet]
@@ -22,31 +25,78 @@ namespace KR1.Controllers
         {
             return View();
         }
+
         [HttpPost]
         public IActionResult Create_New_Application(Application application)
         {
-            application.ApplicationDate = DateTime.Now;
-            application.Status = "New";
-            db.Applications.Add(application);
-            db.SaveChanges();
-            return Redirect("/Home/Index/");
+                application.ApplicationDate = DateTime.Now;
+                application.Status = "New";
+                _db.Applications.Add(application);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+
         }
 
-        private void Alert(string v)
+        [HttpGet]
+        public IActionResult Edit_New_Application(int? id)
         {
-            throw new NotImplementedException(v);
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var application = _db.Applications.Find(id);
+            if (application == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            ViewData["ConfirmEditResult"] = false;
+            return View(application);
         }
 
-        public enum TimeOfDay
+        [HttpPost]
+        public IActionResult Edit_New_Application(int id, bool confirmEditResult)
         {
-            [Display(Name = "Утро")]
-            Morning,
-            [Display(Name = "День")]
-            Afternoon,
-            [Display(Name = "Вечер")]
-            Evening,
-            [Display(Name = "Ночь")]
-            Night
+            var application = _db.Applications.Find(id);
+            if (application != null)
+            {
+                application.Status = confirmEditResult ? "Confirmed" : "Cancel";
+                _db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public IActionResult Delete_New_Application(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var application = _db.Applications.Find(id);
+            if (application == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View(application);
+        }
+
+        [HttpPost]
+        public IActionResult Delete_New_Application(int id)
+        {
+            var application = _db.Applications.Find(id);
+            if (application != null)
+            {
+                _db.Applications.Remove(application);
+                _db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
